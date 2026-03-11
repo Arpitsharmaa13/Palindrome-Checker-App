1cef8f0c31f987bd165e717ab52b6ec541108357
Arpitsharmaa13 <arpitsharma232007@gmail.com>
2026-03-12 04:59:16 +0530

    UC1:Add welcome message module

--- src/palindrome.java
+++ src/palindrome.java
@@ -1,2 +1,11 @@
 public class palindrome {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("   Welcome to Palindrome App");
+        System.out.println("=================================");
+
+    }
+
 }
